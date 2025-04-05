e55ef5aea8a3ec15852273bd10055fa1f72e8bd8
Augusto Reis <devaugustoreis@gmail.com>
2025-04-05 16:27:06 -0300

    feat: backend migration

---
+++ gerenciador.dentedeleao/src/test/java/com/gerenciadordentedeleao/GerenciadorDenteDeLeaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciadordentedeleao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDenteDeLeaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
