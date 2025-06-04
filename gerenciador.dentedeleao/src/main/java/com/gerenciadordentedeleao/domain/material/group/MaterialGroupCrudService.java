45e1224358354392526fbe10b291fbfde07a0db7
arthur <arthurflcorrea@gmail.com>
2025-06-04 18:19:55 -0300

    Refactor save method in MaterialGroupCrudService to improve item handling and ensure correct association with MaterialGroupEntity

--- gerenciador.dentedeleao/src/main/java/com/gerenciadordentedeleao/domain/material/group/MaterialGroupCrudService.java
+++ gerenciador.dentedeleao/src/main/java/com/gerenciadordentedeleao/domain/material/group/MaterialGroupCrudService.java
@@ -4,7 +4,7 @@ import com.gerenciadordentedeleao.application.abstractions.AbstractCrudService;
 import com.gerenciadordentedeleao.domain.material.group.item.MaterialGroupItemKey;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class MaterialGroupCrudService extends AbstractCrudService<MaterialGroupEntity> {
@@ -20,17 +20,16 @@ public class MaterialGroupCrudService extends AbstractCrudService<MaterialGroupE
 
     @Override
     public MaterialGroupEntity save(MaterialGroupEntity entity) {
-        var items = entity.getItems();
-        entity.setItems(new ArrayList<>());
+        var items = List.copyOf(entity.getItems());
+        entity.getItems().clear(); // Clear the existing collection
         var savedEntity = repository.saveAndFlush(entity);
-        var materialGroup = new MaterialGroupEntity();
-        materialGroup.setId(savedEntity.getId());
 
         items.forEach(item -> {
             item.setMaterialGroupItemId(new MaterialGroupItemKey(savedEntity.getId(), item.getMaterial().getId()));
-            item.setMaterialGroup(materialGroup);
-        } );
-        savedEntity.setItems(items);
+            item.setMaterialGroup(savedEntity);
+            savedEntity.getItems().add(item); // Add the new items to the existing collection
+        });
+
         return repository.save(savedEntity);
     }
 }
